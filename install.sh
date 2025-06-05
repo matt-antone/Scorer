46108a2f4dec9e7d8b37a6de1d4bb67e21890322
Matthew Antone <matt.antone@gmail.com>
2025-06-04 21:01:13 -0700

    updated

--- install.sh
+++ install.sh
@@ -85,7 +85,7 @@ echo "--- Installing SDL2 build dependencies ---"
 sudo apt install -y build-essential git autoconf automake libtool pkg-config \
     libasound2-dev libpulse-dev libaudio-dev libx11-dev libxext-dev \
     libxrandr-dev libxcursor-dev libxi-dev libxinerama-dev libxxf86vm-dev \
-    libxss-dev libgl1-mesa-dev libesd0-dev libdbus-1-dev libudev-dev \
+    libxss-dev libgl1-mesa-dev libdbus-1-dev libudev-dev \
     libgles2-mesa-dev libegl1-mesa-dev libibus-1.0-dev \
     libdrm-dev libgbm-dev libinput-dev libudev-dev libxkbcommon-dev
 
